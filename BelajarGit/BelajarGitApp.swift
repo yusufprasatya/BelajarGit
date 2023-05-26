7fb10ecd3810f7d70cb0d5da877144ea2dbec391
yusufprasatya <yusufmarsudien10@gmail.com>
2023-05-26 15:41:48 +0700

    Initial Commit

---
+++ BelajarGit/BelajarGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelajarGitApp.swift
+//  BelajarGit
+//
+//  Created by Muhammad Yusuf on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BelajarGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
