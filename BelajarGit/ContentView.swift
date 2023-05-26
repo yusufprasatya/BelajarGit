331766525d781e7a4993f48eae1659c694043cdc
yusufprasatya <yusufmarsudien10@gmail.com>
2023-05-26 15:53:04 +0700

    add main feature

--- BelajarGit/ContentView.swift
+++ BelajarGit/ContentView.swift
@@ -1,26 +1,68 @@
 //
 //  ContentView.swift
-//  BelajarGit
+//  GIT_Collaboration_Test
 //
-//  Created by Muhammad Yusuf on 26/05/23.
+//  Created by Muhammad Yusuf on 15/05/23.
 //
 
 import SwiftUI
 
+// Orang ke 1
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Awesome List")
         }
-        .padding()
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListRowView: View {
+    var body: some View {
+        NavigationLink(destination: DetailView()) {
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.blue)
+                
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: 80, height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                }
+                .foregroundColor(.orange)
+            }
+            .padding(.vertical, 10)
+        }
     }
 }
